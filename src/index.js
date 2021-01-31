6e24a99c35352005ea035bbb1a514727d3c53dec
AnaMoskvina <moskvinaanastasia@gmail.com>
2021-01-31 12:10:22 +0300

    feat: implement function check

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const samples = bracketsConfig.flatMap(item => item.join(""));
+  let counter = 0;
+
+  for (let i = 0; i < samples.length; i++) {
+    if(!str.includes(samples[i])) counter++;
+    else str = str.replace(samples[i], "");
+  }
+
+  if (counter === samples.length) return false;
+
+  if (str.length === 0) return true;
+
+  return check(str, bracketsConfig);
 }
+
+
